f1acb7ee13d36b24b297d31b832d5be4f35fc9e6
Mafrans <malte.kluft@gmail.com>
2023-09-05 23:31:58 +0200

    fix: hardcode branch name

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 # Download GoblinAI
 git clone https://github.com/Mafrans/GoblinAI.git
 cd GoblinAI
-git pull origin $1
-git checkout $1
+git pull origin dev
+git checkout dev
 
 # Install PythonPoetry if it doesn't already exist
 if ! type "poetry" > /dev/null; then
-  curl -sSL https://install.python-poetry.org | POETRY_HOME=$(pwd)/.bin python3 -
+  curl -sSL https://install.python-poetry.org | POETRY_HOME=$(pwd)/.lib/poetry python3 -
   export PATH=$HOME/.local/bin:$PATH
   hash -r
 fi
