4a601d293406628dd2982cdd8c141b0eadac7fb3
Mafrans <malte.kluft@gmail.com>
2023-09-11 13:43:03 +0200

    feat: add tunnelmole in vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,21 @@
-import { defineConfig } from 'vite'
-import solid from 'vite-plugin-solid'
+import { tunnelmole } from "tunnelmole";
+import { defineConfig } from "vite";
+import solid from "vite-plugin-solid";
 
-export default defineConfig({
-  plugins: [solid()],
-})
+const port = 5173;
+
+export default defineConfig(({ command }) => {
+  if (command === "serve") {
+    tunnelmole({ port }).then((url) => {
+      console.clear();
+      console.log(`GoblinAI running on ${url}`);
+    });
+  }
+
+  return {
+    server: {
+      port,
+    },
+    plugins: [solid()],
+  };
+});
