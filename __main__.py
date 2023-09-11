627ea80db76790b0abb1ad24504f64a0d053c505
Mafrans <malte.kluft@gmail.com>
2023-09-11 15:08:57 +0200

    feat: add ngrok

--- __main__.py
+++ __main__.py
@@ -4,10 +4,12 @@ import argparse
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-m", "--mode", type=str, default="production")
 argParser.add_argument("-D", "--dev", action="store_true")
+argParser.add_argument("--ngrok-token", type=str)
 
 args = argParser.parse_args();
 
-if __name__ == '__main__':  
+if __name__ == '__main__':
     goblinai.start(
-        mode="development" if args.dev else args.mode
+        mode="development" if args.dev else args.mode,
+        ngrokToken=args.ngrok_token
     )
--- goblinai.py
+++ goblinai.py
@@ -1,12 +1,19 @@
 from multiprocessing import Process
 import server
 import client
+import ngrok
 
-def start(mode: str):
+def start(mode: str, ngrokToken: str):
     if (mode == "development"):
         client.startDevServer()
     else:
         # Let the API server handle serving the client
         client.buildClient()
 
+    if (ngrokToken):
+        ngrokTunnel(8080, ngrokToken)
+
     server.start()
+
+def ngrokTunnel(port: int, token: str):
+    ngrok.connect(port, authtoken=token)
