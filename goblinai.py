42f67233eec84ff40d239cc18a094c472750a0b6
Mafrans <malte.kluft@gmail.com>
2023-09-12 18:12:35 +0200

    feat: add server dev mode

--- goblinai.py
+++ goblinai.py
@@ -1,14 +1,15 @@
 import server
 import client
 
+
 def start(mode: str, ngrokToken: str):
-    if (mode == "development"):
+    if mode == "development":
         client.startDevServer()
     else:
         # Let the API server handle serving the client
         client.buildClient()
 
-    server.start()
+    server.start(mode)
 
-    if (ngrokToken != None):
+    if ngrokToken != None:
         server.tunnel(ngrokToken)
--- server/__init__.py
+++ server/__init__.py
@@ -12,35 +12,45 @@ app = FastAPI()
 corsMethods = ["get"]
 corsHeaders = ["*"]
 
-app.add_middleware(CORSMiddleware,
+app.add_middleware(
+    CORSMiddleware,
     allow_origins=[f"http://localhost:5173"],
     allow_methods=corsMethods,
     allow_headers=corsHeaders,
 )
 
+
 @app.get("/api")
 def get_root():
-    return {
-        "text": "Hello world!"
-    }
+    return {"text": "Hello world!"}
 
-def start():
+
+def start(mode: str):
     clientPath = Path(__file__).parents[1].joinpath("client/dist")
 
-    if (clientPath.exists()):
+    if clientPath.exists():
         app.mount("/", StaticFiles(directory=clientPath.absolute(), html=True))
 
-    Process(target=lambda:uvicorn.run(app, port=port, log_level="error")).start()
+    Process(
+        target=lambda: uvicorn.run(
+            "server:app",
+            port=port,
+            log_level="error",
+            reload=(mode == "development"),
+        )
+    ).start()
 
     print(f"GoblinAI running on http://localhost:8000")
+
 
 def tunnel(token: str):
     tunnel = ngrok.connect(port, authtoken=token)
 
-    app.add_middleware(CORSMiddleware,
+    app.add_middleware(
+        CORSMiddleware,
         allow_origins=[tunnel.url()],
         cors_methods=corsMethods,
-        cors_headers=corsHeaders
+        cors_headers=corsHeaders,
     )
-    
+
     print(f"GoblinAI running on {tunnel.url()}")
