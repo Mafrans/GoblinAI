a02990514666a7e90c690ce62c1c6a99d363c0b5
Mafrans <malte.kluft@gmail.com>
2023-09-14 13:37:11 +0200

    wip: autocomplete

--- __main__.py
+++ __main__.py
@@ -1,15 +1,19 @@
+#!/usr/bin/env python
+# PYTHON_ARGCOMPLETE_OK
+
 import goblinai
 import argparse
+import argcomplete
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-m", "--mode", type=str, default="production")
 argParser.add_argument("-D", "--dev", action="store_true")
 argParser.add_argument("--ngrok-token", type=str)
 
+argcomplete.autocomplete(argParser)
 args = argParser.parse_args()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     goblinai.start(
-        mode="development" if args.dev else args.mode,
-        ngrokToken=args.ngrok_token
+        mode="development" if args.dev else args.mode, ngrokToken=args.ngrok_token
     )
